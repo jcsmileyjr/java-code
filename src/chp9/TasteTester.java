3f5208052236de5f9a0e620cb407e45f5896358a
JC <jcsmileyjr@gmail.com>
2020-02-15 18:14:14 -0600

    Refactor the message printed and include the instructions of the app.

--- src/chp9/TasteTester.java
+++ src/chp9/TasteTester.java
@@ -1,13 +1,19 @@
 package chp9;
+/*
+Cake Cake Cake
+Create an application that has a super-class named Cake. Then have two sub
+classes named BirthdayCake and WeddingCake. Then create a TasteTaster class
+that test out the inheritance of the sub classes.
+ */
 
 public class TasteTester {
-    public static void main(String args) {
+    public static void main(String [] args) {
         BirthdayCake birthday = new BirthdayCake("Chocolate");
         birthday.setPrice(100.00);
-        System.out.println("The flavor of the $" + birthday.getPrice() + " Birthday cake is " + birthday.getFlavor());
+        System.out.println("The flavor of the $" + String.format("%.2f", birthday.getPrice()) + " Birthday cake is " + birthday.getFlavor());
 
         WeddingCake wedding = new WeddingCake("Vanilla");
         wedding.setPrice(500);
-        System.out.println("The flavor of the $" + wedding.getPrice() + "Wedding cake is " + wedding.getFlavor());
+        System.out.println("The flavor of the $" + String.format("%.2f", wedding.getPrice()) + " Wedding cake is " + wedding.getFlavor());
     }
 }
